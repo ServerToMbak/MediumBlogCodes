5074f26b31f29922444520dc73cb99418d460bdf
Ext.Server Tombak <ext.server.tombak@sgs.com>
2024-03-22 21:47:07 +0300

    Factory Method and Abstract Factory desgn patterns

--- FactoryDesignPattern/Program.cs
+++ FactoryDesignPattern/Program.cs
@@ -1,97 +1,22 @@
-﻿public class Program
+﻿using FactoryDesignPattern.RerstaurantExample;
+
+public class Program
 {
     private static void Main(string[] args)
     {
-        IWidgetFactory motifFactory = new MotifWidgetFactory();
-        IWindow motifWindow = motifFactory.CreateWindow();
-        IScrollBar motifScrollBar = motifFactory.CreateScrollBar();
 
-        motifWindow.CreateWindow();
-        motifScrollBar.CreateScrollBar();
+        // Eating on Reasturan A
+        RestaurantFactory restaurantA = new RestaurantA();
+        restaurantA.OrderBurger("veggie");
+        restaurantA.OrderPizza();
 
-        // PM platformu için fabrika oluştur
-        IWidgetFactory pmFactory = new PMWidgetFactory();
-        IWindow pmWindow = pmFactory.CreateWindow();
-        IScrollBar pmScrollBar = pmFactory.CreateScrollBar();
 
-        pmWindow.CreateWindow();
-        pmScrollBar.CreateScrollBar();
+        // Eating on Restaurant B
+        RestaurantFactory restaurantB = new RestaurantB();
+        IBurger burger = restaurantB.OrderBurger("veggie");
+        restaurantB.OrderPizza();
+
     }
 
-    interface IWindow
-    {
-        void CreateWindow();
-    }
-
-    class PMWindow : IWindow
-    {
-        public void CreateWindow()
-        {
-            Console.WriteLine("You have created PM Window");
-        }
-    }
-
-    class MotifWindow : IWindow
-    {
-        void IWindow.CreateWindow()
-        {
-            Console.WriteLine("You have created a Motif winow");
-        }
-    }
-
-
-    interface IScrollBar
-    {
-        void CreateScrollBar();
-    }
-
-    class MotifScrollBar : IScrollBar
-    {
-        public void CreateScrollBar()
-        {
-            Console.WriteLine("You have created a  Motif scrollbar ");
-        }
-    }
-
-    class PMScrollBar : IScrollBar
-    {
-        public void CreateScrollBar()
-        {
-            Console.WriteLine("You have created a  PM scrollbar ");
-        }
-    }
-
-    interface IWidgetFactory
-    {
-         IScrollBar CreateScrollBar();
-
-         IWindow CreateWindow();
-    }
-
-
-    class MotifWidgetFactory : IWidgetFactory
-    {
-        public IScrollBar CreateScrollBar()
-        {
-            return new MotifScrollBar();    
-        }
-
-        public IWindow CreateWindow()
-        {
-            return new MotifWindow();
-        }
-    }
-
-    class PMWidgetFactory : IWidgetFactory
-    {
-        public IScrollBar CreateScrollBar()
-        {
-            return new PMScrollBar();
-        }
-
-        public IWindow CreateWindow()
-        {
-            return new PMWindow();
-        }
-    }
+   
 }
---
+++ FactoryDesignPattern/RerstaurantExample/Class1.cs
@@ -0,0 +1,166 @@
+﻿namespace FactoryDesignPattern.RerstaurantExample
+{
+    public  class Class1
+    {
+    }
+
+
+    #region Factory Method Design Pattern section
+        //interface IBurger
+        //{
+
+        //    void CookBurger();
+        //}
+
+        //class ChessBurger : IBurger
+        //{
+        //    public void CookBurger()
+        //    {
+        //        Console.WriteLine("Cooking a Chess Burger");
+        //    }
+        //}
+
+        //class VeggieBurger : IBurger
+        //{
+        //    public void CookBurger()
+        //    {
+        //        Console.WriteLine("Cooking a veggie Burger");
+        //    }
+        //}
+
+        //abstract class BurgerFactory
+        //{
+        //    public abstract IBurger MakeBurger(string type);
+
+        //    public IBurger OrderBurger(string type)
+        //    {
+        //        IBurger burger = MakeBurger(type);
+        //        burger.CookBurger();
+        //        return burger;
+        //    }
+        //}
+
+        //class RestaurantA : BurgerFactory
+        //{
+        //    public override IBurger MakeBurger(string type)
+        //    {
+        //        Console.WriteLine("Cooking on RestaurantA");
+        //        if (type.ToLower() == "veggie")
+        //         {
+        //            return new VeggieBurger();
+        //         }
+        //         return new ChessBurger();
+        //    }
+        //}
+
+
+        //class RestaurantB : BurgerFactory
+        //{
+        //    public override IBurger MakeBurger(string type)
+        //    {
+        //        Console.WriteLine("Cooking on RestaurantB");
+        //        if (type.ToLower() == "veggie")
+        //        {
+        //            return new VeggieBurger();
+        //        }
+        //        return new ChessBurger();
+        //    }
+        //}
+    #endregion
+
+
+
+    #region Abstract Factory Design Pattern - section
+    interface IBurger
+    {
+        void CookBurger();
+    }
+    interface IPizza
+    {
+        void CookPizza();
+    }
+
+    class Margheritapizza : IPizza
+    {
+        public void CookPizza()
+        {
+            Console.WriteLine("Cooking a Margherita Pizza");
+        }
+    }
+
+
+    class ChessBurger : IBurger
+    {
+        public void CookBurger()
+        {
+            Console.WriteLine("Cooking a Chess Burger");
+        }
+    }
+
+    class VeggieBurger : IBurger
+    {
+        public void CookBurger()
+        {
+            Console.WriteLine("Cooking a veggie Burger");
+        }
+    }
+
+    abstract class RestaurantFactory
+    {
+        public abstract IBurger MakeBurger(string type);
+        public abstract IPizza MakePizza();
+        public IPizza OrderPizza()
+        {
+            IPizza pizza = MakePizza();
+            pizza.CookPizza();
+            return pizza;
+        }
+
+        public IBurger OrderBurger(string type)
+        {
+            IBurger burger = MakeBurger(type);
+            burger.CookBurger();
+            return burger;
+        }
+    }
+
+    class RestaurantA : RestaurantFactory
+    {
+        public override IBurger MakeBurger(string type)
+        {
+            Console.WriteLine("Cooking on RestaurantA");
+            if (type.ToLower() == "veggie")
+            {
+                return new VeggieBurger();
+            }
+            return new ChessBurger();
+        }
+
+        public override IPizza MakePizza()
+        {
+            Console.WriteLine("Cooking on RestaurantA");
+            return new Margheritapizza();
+        }
+    }
+
+
+    class RestaurantB : RestaurantFactory
+    {
+        public override IBurger MakeBurger(string type)
+        {
+            Console.WriteLine("Cooking on RestaurantB");
+            if (type.ToLower() == "veggie")
+            {
+                return new VeggieBurger();
+            }
+            return new ChessBurger();
+        }
+
+        public override IPizza MakePizza()
+        {
+            Console.WriteLine("Cooking on RestaurantB");
+            return new Margheritapizza();
+        }
+    }
+    #endregion
+}
